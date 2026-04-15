5e1f0e182c6959ad44cb054d502f32bec8447f0c
Atit Manandhar <66715983+atitmanandhar@users.noreply.github.com>
2026-04-15 16:17:47 +0545

    Change metadata value

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,15 +12,15 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://nim-fawn.vercel.app/'),
+  metadataBase: new URL('https://atitmanandhar.com.np/'),
   alternates: {
     canonical: '/'
   },
   title: {
-    default: 'Nim - Personal website template',
+    default: 'Atit Manandhar',
     template: '%s | Nim'
   },
-  description:  'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
+  description:  'Atit Manandhar. Write blogs and share projects I worked on.',
 };
 
 const geist = Geist({
