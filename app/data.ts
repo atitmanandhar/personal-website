0b060efc3edf4e12b065329ea06ab497b1be983e
Atit Manandhar <66715983+atitmanandhar@users.noreply.github.com>
2026-05-08 10:44:12 +0545

    Fix mdx format issue

--- app/data.ts
+++ app/data.ts
@@ -29,12 +29,12 @@ type SocialLink = {
 
 export const PROJECTS: Project[] = [
   {
-    name: 'Motion Primitives Pro',
+    name: 'Valley Ornaments Website',
     description:
-      'Advanced components and templates to craft beautiful websites.',
-    link: 'https://pro.motion-primitives.com/',
+      'A client website built using React JS and Express.',
+    link: 'https://valleyornaments.com.np/',
     video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+      'https://www.youtube.com/embed/EpGi_SIxjtQ?si=TN0O7xagwPsKFLVw',
     id: 'project1',
   },
   {
